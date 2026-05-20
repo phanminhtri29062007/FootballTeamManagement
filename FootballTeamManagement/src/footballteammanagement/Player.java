add0555174dc1e43d31a495ca683d4b467d78e84
tripm <phanminhtri29062007@gmail.com>
2026-05-20 15:55:37 +0000

    Initialized classes. Class players contain players info and Training Sessions contain session infos No progresses toward methods yet

---
+++ FootballTeamManagement/src/footballteammanagement/Player.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package footballteammanagement;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author phanm
+ */
+public class Player {
+    long playerID;
+    String fullName;
+    int age;
+    String nationality;
+    String position;
+    int shirtNumber;
+    int baseSalary;
+    int playerType;
+    boolean status;
+    void enterPlayer(){
+        
+    }
+    void printPlayer(){
+        
+    }
+}
+class playerList {
+    ArrayList<Player> list;
+    int count;
+    void updatePlayerInfo(){
+        
+    }
+    void deactivatePlayer(){
+        
+    }
+    void printAllPlayer(){
+        
+    }
+    void searchPlayer(){
+        
+    }
+    void printPlayerInfo(){
+        
+    }
+}
---
+++ FootballTeamManagement/src/footballteammanagement/TrainingSession.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package footballteammanagement;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author phanm
+ */
+public class TrainingSession {
+    long trainingID;
+    int[] date= new int[3];
+    String location;
+    String topic;
+    ArrayList<Long> presentID;
+
+    public TrainingSession() {
+    }
+    void recordSession(){
+        
+    }
+    void printSessionRecord(){
+        
+    }
+}
+
+class trainingHistory {
+    ArrayList<TrainingSession> trainingList;
+    void addSession(){
+        
+    }
+    void viewHistory(){
+        
+    }
+}
