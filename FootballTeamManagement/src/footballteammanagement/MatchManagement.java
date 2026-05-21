69bcb4b69dad3fc08c5e92bebc68110e9b2497d8
Haruu3123 <91402869+Haruu3123@users.noreply.github.com>
2026-05-21 16:12:31 +0700

    Initial commit of my project folder

---
+++ FootballTeamManagement/src/footballteammanagement/MatchManagement.java
@@ -0,0 +1,17 @@
+package FootballTeamManagement;
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nguyenlongvu
+ */
+public class MatchManagement {
+        double MatchID;
+        
+        
+}
