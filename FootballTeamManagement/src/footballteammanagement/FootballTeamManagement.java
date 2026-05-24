a7950ae658ffed3e8cd2b884c856ad66805a2bc6
Haruu3123 <91402869+Haruu3123@users.noreply.github.com>
2026-05-24 23:40:57 +0700

    Update Match.Java

--- FootballTeamManagement/src/footballteammanagement/FootballTeamManagement.java
+++ FootballTeamManagement/src/footballteammanagement/FootballTeamManagement.java
@@ -5,75 +5,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FootballTeamManagement {
-     static Scanner sc = new Scanner(System.in);
-
-     static String inputString(String prompt){
-        String value;
-        while(true){
-            System.out.print(prompt);
-            value = sc.nextLine().trim().toLowerCase();
-            if(!value.isEmpty()){
-                return value;
-            }
-            System.out.println("Input can't be empty.Please try again! ");
-        }
-     }
-
-      static boolean isValidPosition(String position){
-        return position.equals("goalkeeper")
-        || position.equals("defender")
-        || position.equals("midfielder")
-        || position.equals("forward");
-
-
-      }
-
-      static int inputInt(String prompt, int min, int max){
-        int value;
-        while(true){
-            System.out.print(prompt);
-            try{ String input = sc.nextLine().trim();
-                value = Integer.parseInt(input);
-               if(value >= min && value <= max){
-                return value;
-               } 
-               System.out.println("Value must between " + min + " and " + max );
-                
-            }
-            catch(NumberFormatException e){
-                System.out.println("Invalid input.Please enter a whole number!");
-            }
-        }
-      }
-
-      static double inputSalary(String prompt){
-        double value;
-        while(true){
-            System.out.print(prompt);
-            try{
-                String input = sc.nextLine();
-                input = input.trim();
-                value = Double.parseDouble(input);
-
-                if(value > 0){
-                    return value;
-                }
-                System.out.print("salary must be greater than 0.");
-            }
-            catch(NumberFormatException e){
-                System.out.println("Invalid input.Please enter a valid number!");
-            }
-        }
-      }
-
-    
     public static void main(String[] args) {
-        System.out.println("Hi");
-        trainingRecords tList= new trainingRecords();
-        tList.trainingList=new ArrayList<>();
-        tList.count=0;
-        tList.addSession(2);
-        tList.viewHistory();
+
+        System.out.println("\n=== RUNNING MATCH DETAILS ===");
+        // Step 1: Create a new Match object
+        Match game1 = new Match();
+
+        // Step 2: Ask the user to input the details
+        game1.MatchDetailsInput();
+
+        // Step 3: Print the details so far (Scores will be 0 - 0)
+        System.out.println("Current Match List:");
+        game1.MatchInformation();
+
+        // Step 4: Ask the user to update the live scores
+        game1.UpdateLiveResult();
+
+        // Step 5: Print the list again to see the updated scores!
+        System.out.println("Updated Match List:");
+        game1.MatchInformation();
     }
-    
-}
+} // Notice there i
--- FootballTeamManagement/src/footballteammanagement/Match.java
+++ FootballTeamManagement/src/footballteammanagement/Match.java
@@ -1,7 +1,7 @@
-package FootballTeamManagement;
-import java.util.ArrayList;
+package footballteammanagement;
 import java.time.LocalDateTime;
-
+import java.util.Scanner;
+        
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,35 +11,118 @@ import java.time.LocalDateTime;
  *
  * @author nguyenlongvu
  */
+//use private data type for encapsulation//
 public class Match {
-        double MatchID;
-        String HomeTeam;
-        String OpponentTeam;
-        String Venue;
-        LocalDateTime MatchDateTime;
-        int HomeScore;
-        int OpponentScore;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       private int MatchID;
+       private String HomeTeam;
+       private String OpponentTeam;
+       private String Venue;
+       private LocalDateTime MatchDateTime;
+       private int HomeScore;
+       private int OpponentScore;
+        void MatchDetailsInput(){
+            MatchDateTime = LocalDateTime.now();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("MatchID:");
+             if(sc.hasNextInt()){ //If input is integer ( valid )
+            MatchID = sc.nextInt();
+            sc.nextLine();
+             } else{ // if input is not integer ( return invalid and we need to input again )
+                 System.out.println("Invalid Match ID");
+                 sc.nextLine();
+             }//This fix consumes leftover "Enter" key//
+            System.out.println("HomeTeam:");
+             HomeTeam = sc.nextLine();
+            if(HomeTeam.isEmpty()){
+            System.out.println("Team Name Cannot Be Empty");
+             HomeTeam = sc.nextLine();
+            } 
+            System.out.println("OpponentTeam:");
+            OpponentTeam = sc.nextLine();
+            //EqualsIgnoreCase methods compare input between opponentTeam and HomeTeam, if OpponentTeam has same name with HomeTeam ( refuse to receive that input )
+             while (OpponentTeam.isEmpty() || OpponentTeam.equalsIgnoreCase(HomeTeam)) {
+            if(OpponentTeam.isEmpty()){
+                System.out.println("TeamName Cannot Be Empty");
+            }
+            else{
+                System.out.println("Opponent Cannot Be The Same With HomeTeam");
+            }
+            OpponentTeam = sc.nextLine();
+            sc.nextLine();
+             }
+            System.out.println("Venue:");
+            Venue = sc.nextLine();
+            if(Venue.isEmpty()){
+                System.out.println("Venue Cannot Be Empty");
+               Venue = sc.nextLine();
+            }
+             //Use LocalDateTime object for input date and time for match details input
+            System.out.println("--Enter Match Date & Time--");
+            System.out.println("Enter Day:");
+            int day = sc.nextInt();
+            while (day < 1 || day > 31){
+                    System.out.println("Day Declined");
+                    day = sc.nextInt();
+            }
+            System.out.println("Enter Month:");
+            int month = sc.nextInt();
+            while (month < 1 || month > 12){
+                System.out.println(" Month Declined");
+                month = sc.nextInt();
+            }
+           
+            System.out.println("Enter Year:");
+            int year = sc.nextInt();
+            while (year < 2000 || year > 2100){
+                System.out.println("Year Declined");
+                year = sc.nextInt();
+            }          
+            System.out.println("Enter Hour:");
+            int hour = sc.nextInt();
+            while (hour < 0 || hour > 23){
+                System.out.println("Hour Declined");
+                hour = sc.nextInt();
+            }
+            System.out.println("Enter Minute:");
+            int minute = sc.nextInt();
+            while (minute < 0|| minute > 59){
+                System.out.println("Minute Declined");
+                minute = sc.nextInt();
+            }
+            sc.nextLine();
+            
+            
+                
+            
+           
 }
-class MatchEvent{
-    void MatchLiveResult(){
-        
-    }
-}
-
-class matchRecord{
+        void UpdateLiveResult(){
+          Scanner sc = new Scanner(System.in);
+          System.out.println("----Update Live Score---");
+          System.out.println("Current Score"+ HomeTeam+":");
+          if(sc.hasNextInt()){
+              HomeScore = sc.nextInt();
+              sc.nextLine();
+          }
+          else{
+              System.out.println("Cannot Update Score For HomeTeam");
+              sc.nextLine();
+          }
+          
+       
+          System.out.println("Current Score"+OpponentTeam+":");
+          if(sc.hasNextInt()){
+              OpponentScore = sc.nextInt();
+              sc.nextLine();
+          }
+          else{
+              System.out.println("Cannot Update Score For OpponentTeam");
+          }         
+        }
     void MatchInformation(){
-        
+          System.out.format("%d | %20s:%d vs %20s:%d |%20s| %td/%tm/%ty %tl.%tM %tp %n", 
+                MatchID, HomeTeam,HomeScore,OpponentTeam,OpponentScore, Venue, 
+                MatchDateTime, MatchDateTime, MatchDateTime, MatchDateTime, MatchDateTime, MatchDateTime);
     }
     
 }
@@ -47,11 +130,12 @@ class Schedule{
         void ScheduleDetails(){
             
         }
-        void ChangeKickOff(){
+        void ChangeSchedules(){
             
         }
 }
 class Team{
+    
     void TeamInformation(){
         
     }
